cae6f96f56e2ffe31e00c339d71e11a377c024c2
Efunnuga Bamidele <bamideleefunnuga9@gmail.com>
2022-11-16 09:48:37 +0100

    feat: Process Management using PM2

--- PERFORMANCE_EXAMPLE/server.js
+++ PERFORMANCE_EXAMPLE/server.js
@@ -10,13 +10,16 @@ function delay(duration){
 }
 
 app.get('/', (req, res) => {
-    res.send('Performance example');
+    res.send(`Performance example: ${process.pid}`);
 });
 
 app.get('/timer', (req, res) => {
     //delay the response
     delay(9000);
-    res.send('Ding ding ding!');
+    res.send(`Ding ding ding!: ${process.pid}`);
 });
 
+console.log('Running server.js...');
+console.log('Worker process has started!')
 app.listen(3000);
+
